7374e2137bf60818fcb34b5bb738e6737755f755
Lucas M. Fraile <lucasfrailev@gmail.com>
2024-03-20 23:44:28 -0700

    clean angular data-

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -147,6 +147,14 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     Hj_ = tools.CalculateJacobian(ekf_.x_);
     ekf_.H_ = Hj_;
     ekf_.R_ = R_radar_;
+    Eigen::VectorXd radar_measurements_ =  VectorXd(3);
+    radar_measurements_ = measurement_pack.raw_measurements_;
+    while (radar_measurements_(1)> (atan(1)*4)){
+      radar_measurements_(1)-= 2 * atan(1)*4;
+    }
+    while (radar_measurements_(1)< (-atan(1)*4)){
+      radar_measurements_(1)+= 2 * atan(1)*4;
+    }
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // TODO: Laser updates
